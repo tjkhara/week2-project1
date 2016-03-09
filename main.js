11b0e67ce4cdb5053b2c7714f048296f9c4aed86
bbouley <b.bouley@hotmail.co.uk>
2016-03-09 10:05:47 -0700

    included stretch goals and split up exercises

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 
 // For any methods that we didn't cover, please refer to the Object Documentation.
 // Please console.log("Question#") followed by console.log(yourAnswer) after each question.
-// If you would like, practice using document.write() as well. 
+// If you would like, practice using document.write() as well.
 // Use any elements you like!
 
 // ==========================================================================
@@ -113,48 +113,4 @@ var invitees = { adults: [{
 
 // Bonus: Explain, in your own words, the difference between Literal Notation and Constructor.
 
-// 17. Let's play a game. Create an object (like we did with the bike in class) and assign it at least five properties.
-// The class will guess what your object is based on it's properties.
-
-// You're a zookeeper and keeping a detailed log of each of the animals in your zoo.
-// 18. Create an empty object called "zoo"
-
-// 19. Create assign three animals as your keys in the "zoo" object. The values of the animals should be empty objects.
-
-// 20. Within each animal empty object, assign a key/value pair of "timeSlept" and a number indicating the hours of sleep each animal got. (Make this up, obvi)
-
-// 21. Within each animal object, assign a object pair consisting of a key called "meals" and an empty array as the value.
-
-// 22. Within each animal's meals array, create two objects with keys of "monday" and "wednesday". You only feed those jerks twice a week. It's fine.
-
-// 23. In the individual days of the week, create an object with the key of each meal ("breakfast", "lunch", "dinner") and a made up value of what they ate.
-// That array should look like this (within the meals object, within the animal object, within the zoo object):
-  [ { monday: {
-    breakfast: "cheetos on top of greek yogurt",
-    lunch: "6 gallons of Diet Coke",
-    dinner: "leftover lasagna"
-  }},
-   {wednesday: {
-    breakfast: "cashews, diced apples, tuna, grapes, BubbleYum",
-    lunch: "a ham and cheddar lunchable",
-    dinner: "I put my thing down, flip it, and reverse it"
-  }}]
-
-// 24. Damn it. The night zookeeper just informed you that your second animal took a nap right before bed time. Add two hours to his "timeSlept" value.
-
-// 25. To each animal object, add a "favoriteActivities" key with the value being an array of three of his/her favorite activities.
-
-// 26. Your first animal just tossed his lunch at the zoo visitors. Change his monday lunch value to an empty string.
-
-// 27. You know your "zoo" object? The owner wants to add zookeepers to the object, too. But before we do that, we need to make sure
-// the animals stay separate from the zookeepers. To do this, create an object within the "zoo" object. The key will be "animals" and the value will be an array
-// containing the three animals you already defined.
-
-// 28. To the "zoo" object, add an object named "zookeepers" with the value being and empty object.
-
-// 29. To the empty "zookeepers" object, add two objects with their keys being two names and their values being an empty object.
-
-// 30. This joker wants you to add the "hireDate" for each zookeeper. Within the empty array attached to each name, assign a key/value pair of "hireDate" and the value being a string of hire date.
-
-    
-    
+//If you've finished with these, move onto the stretch file to find more exercises using objects
