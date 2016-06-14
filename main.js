b0e3d10fa9432629dbe78b3c544ac6ea9fbe8828
Jenny Allar <jennyallar@gmail.com>
2016-06-14 08:44:08 -0600

    correcting books array to books object

--- main.js
+++ main.js
@@ -95,15 +95,15 @@ var invitees = { adults: [{
 
 
 // ADVANCED TRACK
-// 14. Create an empty array called "books".
+// 14. Create an empty object called "books".
 
 // 15. Create three book objects that contain book titles, their author, and number of pages.
 //     The name of each object should be some kind of made up ISBN number.
 //     (Feel free to make up the titles. And the authors. Have fun. No pressure.)
-//     Insert the three objects into the "books" array.
+//     Insert the three objects into the "books" object.
 
 // Final desired result:
-    // var books = [ISBN22222:  {
+    // var books = {ISBN22222:  {
     //                             title: "You Had Me at Woof",
     //                             author: "Julie Klum",
     //                             pages: 207
@@ -118,7 +118,7 @@ var invitees = { adults: [{
     //                             author: "John Burdett",
     //                             pages: 569,
     //                           }
-    //             ]
+    //             }
 
 // 16. Access the author of the first book.
 
@@ -127,6 +127,6 @@ var invitees = { adults: [{
 // 18. To each book object variable, add a key of "dateRead" with a value of the year you read the book.
 //     (Go ahead and make this up, too. Get wild.)
 
-// 19. Remove the second book from the books Array.
+// 19. Remove the second book from the books object.
 
 // 20. Explain, in your own words, the difference between Literal Notation and Constructor Notation.
